a2c8dc825c921b94d9b7a881d79ca5fa4ea1cc26
Aisamuddin <aisam0094@gmail.com>
2023-04-05 20:14:14 +0800

    feat: improve landing page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,61 +16,67 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="flex flex-col w-full md:flex-row md:justify-between items-center bg-primary shadow-lg text-white py-3 px-2">
-      {/* Logo and company */}
-      <div className="flex items-center">
-        <img
-          className="h-9 w-9 mx-4 hidden md:inline cursor-pointer hover:scale-110"
-          src="/icons/community.svg"
-          onClick={() => navigate("/")}
-        />
+    <nav className="bg-primary shadow">
+      <div className="page-width-container flex flex-col w-full md:flex-row md:justify-between items-center  shadow-lg text-white py-3 px-2">
+        {/* Logo and company */}
+        <div className="flex items-center">
+          <img
+            className="h-9 w-9 mx-4 hidden md:inline cursor-pointer hover:scale-110"
+            src="/icons/community.svg"
+            onClick={() => navigate("/")}
+          />
 
-        {userInfo && !isExpired(token) && (
-          <h1 className="text-lg">{userInfo.company}</h1>
-        )}
-      </div>
+          {userInfo && !isExpired(token) ? (
+            <h1 className="text-lg">{userInfo.company}</h1>
+          ) : (
+            <h1 className="text-xl font-bold uppercase">EMS</h1>
+          )}
+        </div>
 
-      <div className="flex ">
-        {/* <NavLink className="mx-2" to="/about">
+        <div className="flex ">
+          {/* <NavLink className="mx-2" to="/about">
           <h1>About</h1>
         </NavLink> */}
-        {!userInfo || !userInfo.length === 0 || isExpired(token) ? (
-          <div className="flex">
-            <NavLink className="mx-2" to="/login">
-              <h1>Sign In</h1>
-            </NavLink>
-            <NavLink className="mx-2 text-accent font-bold" to="/register">
-              <h1>Free Sign Up </h1>
-            </NavLink>
-          </div>
-        ) : (
-          <div className="flex">
-            <NavLink className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all" to="/department">
-              <img
-                className="flex h-6 w-6"
-                src="/icons/dept.svg"
-              />
-              <h1>Departments</h1>
-            </NavLink>
-            <NavLink className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all" to="/roles">
-              <img
-                className="flex h-5 w-5"
-                src="/icons/role.svg"
-              />
-              <h1>Roles</h1>
-            </NavLink>
-            <NavLink className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all" to="/record">
-              <img
-                className="flex h-6 w-6"
-                src="/icons/employees.svg"
-              />
-              <h1>Employees</h1>
-            </NavLink>
-            <button className="text-accent font-bold mx-2" onClick={logOut}>
-              Logout
-            </button>
-          </div>
-        )}
+          {!userInfo || !userInfo.length === 0 || isExpired(token) ? (
+            <div className="flex items-center">
+              <NavLink className="mx-2" to="/login">
+                <h1>Sign In</h1>
+              </NavLink>
+              <NavLink className="mx-2 font-bold" to="/register">
+                <h1 className="shadow border-none text-black rounded px-4 py-2 bg-tertiary hover:bg-tertiary-focus uppercase">
+                  Try for free
+                </h1>
+              </NavLink>
+            </div>
+          ) : (
+            <div className="flex">
+              <NavLink
+                className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all"
+                to="/department"
+              >
+                <img className="flex h-6 w-6" src="/icons/dept.svg" />
+                <h1>Departments</h1>
+              </NavLink>
+              <NavLink
+                className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all"
+                to="/roles"
+              >
+                <img className="flex h-5 w-5" src="/icons/role.svg" />
+                <h1>Roles</h1>
+              </NavLink>
+              <NavLink
+                className="flex items-center mx-2 space-x-1 hover:scale-110 transition-all"
+                to="/record"
+              >
+                <img className="flex h-6 w-6" src="/icons/employees.svg" />
+                <h1>Employees</h1>
+              </NavLink>
+              <button className="text-accent font-bold mx-2" onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
--- src/pages/frontpage.jsx
+++ src/pages/frontpage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import Footer from "../components/footer";
 
 const FrontPage = () => {
   const navigate = useNavigate();
@@ -15,22 +16,74 @@ const FrontPage = () => {
   // }, [navigate, userInfo]);
 
   return (
-    <main className="flex flex-col items-center mb-12">
-      <h1 className="font-bold text-3xl mt-10 text-center">
-        Manage your employee's data with simplicity
-      </h1>
-      <p className="text-xl text-justify mx-auto my-10 w-2/3 md:w-1/2 md:leading-loose">
-        Fulfill your organization's management needs with this website. Gain
-        better understanding of your entire workforce with a proper system. Get
-        started by creating an account or login if you already have one.
-      </p>
-      <NavLink
-        to="/register"
-        className="m-2 p-5 text-white bg-secondary  hover:bg-secondary-focus"
-      >
-        SIGN UP FOR FREE
-      </NavLink>
-    </main>
+    <div>
+      <main className="flex flex-col items-center">
+        <div className="bg-accent-focus w-full">
+          <div className="page-width-container flex flex-col items-center w-full">
+            <h1 className="font-bold text-5xl mt-10 text-center">
+              Manage your employees effortlessly
+            </h1>
+            <p className="text-xl text-center mx-auto my-10 w-2/3 md:w-1/2 md:leading-loose">
+              Fulfill your organization's management needs with this website.
+              Gain better understanding of your entire workforce with a proper
+              system. Start by creating an account or login if you already have
+              one.
+            </p>
+            <NavLink
+              to="/register"
+              className="m-2 py-5 px-12 font-bold text-white bg-secondary hover:bg-secondary-focus rounded-lg uppercase"
+            >
+              SIGN UP FOR FREE
+            </NavLink>
+            <span className="mb-4">It is free to try. No payment needed.</span>
+          </div>
+        </div>
+        {/* Further description */}
+        <div className="bg-gray w-full">
+          <div className="page-width-container w-full flex flex-col items-center">
+            {/* One */}
+            <div className="py-8 flex justify-between w-full">
+              <div className="frontpage-context flex flex-col mx-4">
+                <span className="text-3xl font-bold">
+                  Create employees record with ease
+                </span>
+                <span className="text-lg">
+                  The system allows you to easily input and track your
+                  employees' data. From employee's name to job titles, every
+                  data properties can be tracked.
+                </span>
+              </div>
+              <div>
+                <img
+                  className="frontpage-context border-4 mx-4"
+                  src="/screenshots/record.png"
+                />
+              </div>
+            </div>
+            {/* Two */}
+            <div className=" py-8 flex justify-between w-full">
+              <div>
+                <img
+                  className="frontpage-context border-4 mx-4"
+                  src="/screenshots/update_employee.png"
+                />
+              </div>
+              <div className="frontpage-context flex flex-col mx-4">
+                <span className="text-3xl font-bold">
+                  Manage your employees
+                </span>
+                <span className="text-lg">
+                  You can update your employees information as needed. You can
+                  assign them to their own respective department and roles in
+                  the company.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
